a8fd2650fdd4930dd45ed10b64377370db0e9c47
Andrew Lee <andyrewlee@gmail.com>
2020-01-25 18:42:39 -0800

    Create room channel

---
+++ server/test/remote_web/channels/room_channel_test.exs
@@ -0,0 +1,26 @@
+defmodule RemoteWeb.RoomChannelTest do
+  use RemoteWeb.ChannelCase
+
+  setup do
+    {:ok, _, socket} =
+      socket(RemoteWeb.UserSocket, "user_id", %{some: :assign})
+      |> subscribe_and_join(RemoteWeb.RoomChannel, "room:lobby")
+
+    {:ok, socket: socket}
+  end
+
+  test "ping replies with status ok", %{socket: socket} do
+    ref = push socket, "ping", %{"hello" => "there"}
+    assert_reply ref, :ok, %{"hello" => "there"}
+  end
+
+  test "shout broadcasts to room:lobby", %{socket: socket} do
+    push socket, "shout", %{"hello" => "all"}
+    assert_broadcast "shout", %{"hello" => "all"}
+  end
+
+  test "broadcasts are pushed to the client", %{socket: socket} do
+    broadcast_from! socket, "broadcast", %{"some" => "data"}
+    assert_push "broadcast", %{"some" => "data"}
+  end
+end
