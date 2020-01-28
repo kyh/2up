db278936295bf9fa40e32b7ea3e6ae0bd410a6f7
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-01-27 21:13:59 -0800

    Code cleanup, add name to message

--- client/src/pages/Remote.tsx
+++ client/src/pages/Remote.tsx
@@ -1,54 +1,43 @@
 import React from 'react';
 import { Button } from 'components';
-import { useChannel } from 'use-phoenix-channel';
-
-const channelName = 'room:lobby';
-const action = 'shout';
-
-type Message = {
-  name: string;
-  message: string;
-};
-
-type State = Array<Message>;
-
-const messageReducer = (
-  state: State,
-  { event, payload }: { event: any; payload: Message }
-) => {
-  switch (event) {
-    case action:
-      return [...state, { name: payload.name, message: payload.message }];
-    default:
-      return state;
-  }
-};
-const initialState: State = [];
+import { useMessages, actions, MessageType } from 'hooks/useMessages';
 
 export const Remote = () => {
-  const [state, broadcast] = useChannel(
-    channelName,
-    messageReducer,
-    initialState
-  );
+  const [state, broadcast] = useMessages();
   const [message, setMessage] = React.useState('');
+  const [name, setName] = React.useState('');
 
   return (
     <div>
       <h2>Remote</h2>
       <div>
-        {state.map((message: Message, i: number) => {
-          return <div key={i}>{message.message}</div>;
+        {state.map((message: MessageType, i: number) => {
+          return (
+            <div key={i}>
+              {message.name}: {message.message}
+            </div>
+          );
         })}
       </div>
-      <input
-        type="text"
-        value={message}
-        onChange={e => setMessage(e.target.value)}
-      />
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Message:
+        <input
+          type="text"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        />
+      </label>
       <Button
         onClick={() => {
-          broadcast(action, { message });
+          broadcast(actions.shout, { name, message });
           setMessage('');
         }}
       >
