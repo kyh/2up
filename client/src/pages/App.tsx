c70d0ebd5565f257fe6cae4129d7a7bbf4e55c93
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-01-29 01:20:49 -0800

    Add in sound effects

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { TV } from 'pages/TV';
 import { Remote } from 'pages/Remote';
 
 export const App: React.FC = () => {
   return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">TV</Link>
-        </li>
-        <li>
-          <Link to="/remote">Remote</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route exact path="/">
-          <TV />
-        </Route>
-        <Route path="/remote">
-          <Remote />
-        </Route>
-      </Switch>
-    </>
+    <Switch>
+      <Route exact path="/">
+        <TV />
+      </Route>
+      <Route path="/remote">
+        <Remote />
+      </Route>
+    </Switch>
   );
 };
--- client/src/pages/TV.tsx
+++ client/src/pages/TV.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { SoundMap } from 'styles/sounds';
+import { Button } from 'components';
 
 export const TV = () => {
   return (
     <div>
       <h2>TV</h2>
+      <Button
+        onClick={() => {
+          const themeSong = new Audio(SoundMap.theme);
+          themeSong.addEventListener('canplaythrough', () => {
+            themeSong.loop = true;
+            themeSong.play();
+          });
+        }}
+      >
+        Music go
+      </Button>
+      <Link to="/remote">Remote</Link>
     </div>
   );
 };
