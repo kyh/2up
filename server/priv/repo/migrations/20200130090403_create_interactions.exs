e707ce3886f06b89547120ac301a5c97cb00920f
Andrew Lee <andyrewlee@gmail.com>
2020-01-30 02:06:59 -0800

    Add migrations for live play

---
+++ server/priv/repo/migrations/20200130090403_create_interactions.exs
@@ -0,0 +1,13 @@
+defmodule Remote.Repo.Migrations.CreateInteractions do
+  use Ecto.Migration
+
+  def change do
+    create table(:interactions) do
+      add :response_id, references(:responses), null: false
+      add :character_id, references(:characters), null: false
+      add :performance_id, references(:performances), null: false
+
+      timestamps()
+    end
+  end
+end
