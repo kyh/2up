d5781ae4d05a2bd5ead2202c01887e912afa8202
Andrew Lee <andyrewlee@gmail.com>
2020-01-29 23:41:27 -0800

    Add initial seed

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,67 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Remote.Repo
+alias Remote.Accounts.User
+alias Remote.Catalog.Play
+alias Remote.Catalog.Scene
+alias Remote.Catalog.Response
+
+user =
+  %User{}
+  |> User.changeset(%{
+      username: "user",
+      email: "user@example.com",
+      password_hash: "password"
+    })
+  |> Repo.insert!
+
+%Play{
+  name: "Hello world",
+  user: user,
+  scenes: [
+    %Scene{
+      prompt: "Which of these quarterbacks have won the Super Bowl?",
+      user: user,
+      responses: [
+        %Response{
+          response: "Tom Brady",
+          correct: true,
+          user: user
+        },
+        %Response{
+          response: "Eli Manning",
+          correct: true,
+          user: user
+        },
+        %Response{
+          response: "Ray Lewis",
+          correct: false,
+          user: user
+        }
+      ] 
+    },
+    %Scene{
+      prompt: "Which of these teams have never won a Super Bowl?",
+      user: user,
+      responses: [
+        %Response{
+          response: "San Francisco 49ers",
+          correct: false,
+          user: user
+        },
+        %Response{
+          response: "Buffalo Bills",
+          correct: true,
+          user: user
+        },
+        %Response{
+          response: "Minnesota Vikings",
+          correct: false,
+          user: user
+        }
+      ] 
+    }
+  ]
+} |> Repo.insert!
