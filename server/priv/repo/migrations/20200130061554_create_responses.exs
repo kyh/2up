7bc0f35b1b74e62b801040ca0533b1ba342ffceb
Andrew Lee <andyrewlee@gmail.com>
2020-01-29 23:40:40 -0800

    Create initial migrations

---
+++ server/priv/repo/migrations/20200130061554_create_responses.exs
@@ -0,0 +1,14 @@
+defmodule Remote.Repo.Migrations.CreateResponses do
+  use Ecto.Migration
+
+  def change do
+    create table(:responses) do
+      add :response, :string, null: false
+      add :correct, :boolean, default: false, null: false
+      add :user_id, references(:users), null: false
+      add :scene_id, references(:scenes), null: false
+
+      timestamps()
+    end
+  end
+end
