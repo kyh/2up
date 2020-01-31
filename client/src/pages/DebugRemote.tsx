72aeb55c152a632462dcce4252d2f54ceed10d85
Andrew Lee <andyrewlee@gmail.com>
2020-01-31 03:16:38 -0800

    Setup debug components for testing

---
+++ client/src/pages/DebugRemote.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react' 
+import { useTrivia } from 'hooks/useTrivia';
+import Debug from 'components/Debug'
+
+export const DebugRemote = () => {
+  const [state, broadcast] = useTrivia()
+  const [name, setName] = useState('remote')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
+  }
+
+  const handleJoin = () => {
+    broadcast('broadcast', { name, message: 'join' })
+  }
+
+  const handleStart = () => {
+    broadcast('broadcast', { name, message: 'start' })
+  }
+
+  const handleNextScene = () => {
+    broadcast('broadcast', { name, message: 'scene:next' })
+  }
+
+  const handleNextAct = () => {
+    broadcast('broadcast', { name, message: 'act:next' })
+  }
+
+  return (
+    <>
+      <Debug state={state} />
+      <div>
+        <input onChange={handleChange} />
+        <button onClick={handleJoin}>Join</button>
+      </div>
+      <div>
+        <button onClick={handleStart}>Start</button>
+      </div>
+      <div>
+        <button onClick={handleNextScene}>Next scene</button>
+      </div>
+      <div>
+        <button onClick={handleNextAct}>Next act</button>
+      </div>
+    </>
+  )
+}
