849edc9cf97ed23e845f0ea176b90e514fd5df21
Andrew Lee <andyrewlee@gmail.com>
2020-02-01 17:03:16 -0800

    Remove unused fns in room channel

--- server/lib/playhouse_web/channels/room_channel.ex
+++ server/lib/playhouse_web/channels/room_channel.ex
@@ -4,22 +4,12 @@ defmodule PlayhouseWeb.RoomChannel do
   require Logger
 
   def join("room:lobby", payload, socket) do
-    if authorized?(payload) do
-      send(self(), :after_join)
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+    send(self(), :after_join)
+    {:ok, socket}
   end
 
   def handle_in("ping", payload, socket) do
     {:reply, {:ok, payload}, socket}
-  end
-
-  def handle_in("shout", payload, socket) do
-    Playhouse.Message.changeset(%Playhouse.Message{}, payload) |> Playhouse.Repo.insert  
-    broadcast socket, "shout", payload
-    {:noreply, socket}
   end
 
   def handle_in("broadcast", %{"message" => "scene1"}, socket) do
@@ -35,44 +25,8 @@ defmodule PlayhouseWeb.RoomChannel do
     {:noreply, socket}
   end
 
-  def handle_in("broadcast", %{"message" => "player:submit"}, socket) do
-    broadcast socket, "player:submit", %{submission: %{content: "DUMMY", name: "NAME"}}
-    {:noreply, socket}
-  end
-
-  def handle_in("broadcast", %{"message" => "join", "name" => name}, socket) do
-    Presence.track(socket, name, %{
-      online_at: inspect(System.system_time(:second))
-    })
-    broadcast socket, "join", %{"players" => Presence.list(socket)}
-    {:noreply, socket}
-  end
-
-  def handle_in("broadcast", %{"message" => "scene:next"}, socket) do
-    broadcast socket, "scene:next", %{"message" => "scene:next"}
-    {:noreply, socket}
-  end
-
-  def handle_in("broadcast", %{"message" => "act:next"}, socket) do
-    broadcast socket, "act:next", %{"message" => "act:next"}
-    {:noreply, socket}
-  end
-
-  def handle_in("broadcast", payload, socket) do
-    broadcast socket, "start", payload
-    {:noreply, socket}
-  end
-
-  defp authorized?(_payload) do
-    true
-  end
-
-  def handle_info(:after_join, socket) do
-    Playhouse.Message.get_messages()
-    |> Enum.each(fn msg -> push(socket, "shout", %{
-        name: msg.name,
-        message: msg.message,
-      }) end)
+  def handle_in("broadcast", %{"message" => "player:submit", "submission" => submission}, socket) do
+    broadcast socket, "player:submit", %{submission: submission}
     {:noreply, socket}
   end
 end
