b3d5de5d474afd05817cebeb0848fe2fbc90ad5f
Andrew Lee <andyrewlee@gmail.com>
2020-02-01 19:35:00 -0800

    Add new models and update seed

---
+++ server/priv/repo/migrations/20200202024055_create_games.exs
@@ -0,0 +1,12 @@
+defmodule Playhouse.Repo.Migrations.CreateGames do
+  use Ecto.Migration
+
+  def change do
+    create table(:games) do
+      add :act, :integer, null: false
+      add :scene, :integer, null: false
+
+      timestamps()
+    end
+  end
+end
