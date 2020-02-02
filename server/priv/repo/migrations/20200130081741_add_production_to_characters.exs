a628922e9c7c6d4ab5932c5f0202a5103cf579d8
Andrew Lee <andyrewlee@gmail.com>
2020-02-01 17:05:08 -0800

    Remove unused models & migrations

--- server/priv/repo/migrations/20200130081741_add_production_to_characters.exs
+++
@@ -1,9 +0,0 @@
-defmodule Playhouse.Repo.Migrations.AddProductionToCharacters do
-  use Ecto.Migration
-
-  def change do
-    alter table(:characters) do
-      add :production_id, references(:productions), null: false
-    end
-  end
-end
