253410b48b64875a2f4e2f92250b9d750886a477
Andrew Lee <andyrewlee@gmail.com>
2020-02-03 23:26:15 -0800

    Simplify seeds

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -11,63 +11,24 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Playhouse.Repo
-alias Playhouse.Play.Game
-alias Playhouse.Play.GameQuestion
 alias Playhouse.Catalog.Question
 alias Playhouse.Catalog.Answer
 
-game = %Game{
-  act: 1,
-  scene: 1,
-  code: "1234"
-} |> Repo.insert!
+questions = [
+  ["Who won the NBA finals in 2003?", "San Antonio Spurs"],
+  ["Who was the 6th president of United States?", "John Quincy Adams"],
+  ["On a QWERTY keyboard, what two letters have raised marks to assist with touch typing?", "F and J"],
+  ["How do you call a group of unicorns?", "A blessing"],
+  ["Who is the oldest man to win People Magazine's sexiest man alive?", "Sean Connery"]
+]
 
-question1 = %Question{
-  content: "Who won the NBA finals in 2003?",
-} |> Repo.insert!
+Enum.each questions, fn question -> 
+  [q | a] = question
 
-%Answer{
-  question: question1,
-  content: "San Antonio Spurs"
-} |> Repo.insert!
-
-question2 = %Question{
-  content: "Who was the 6th president of United States?",
-} |> Repo.insert!
-
-%Answer{
-  question: question2,
-  content: "John Quincy Adams"
-} |> Repo.insert!
-
-question3 = %Question{
-  content: "On a QWERTY keyboard, what two letters have raised marks to assist with touch typing?",
-} |> Repo.insert!
-
-%Answer{
-  question: question3,
-  content: "F and J"
-} |> Repo.insert!
-
-question4 = %Question{
-  content: "How do you call a group of unicorns?",
-} |> Repo.insert!
-
-%Answer{
-  question: question4,
-  content: "A blessing"
-} |> Repo.insert!
-
-question5 = %Question{
-  content: "Who is the oldest man to win People Magazine's sexiest man alive?",
-} |> Repo.insert!
-
-%Answer{
-  question: question5,
-  content: "Sean Connery"
-} |> Repo.insert!
-
-%GameQuestion{
-  game: game,
-  question: question1
-} |> Repo.insert!
+  %Answer{
+    question: %Question{
+      content: q
+    },
+    content: Enum.at(a, 0)
+  } |> Repo.insert!
+end
