df3b12f094244d5481d4a396e9da9532b151c8a5
Andrew Lee <andyrewlee@gmail.com>
2020-02-05 19:41:30 -0800

    Prepare app for deployment

--- server/config/prod.secret.exs
+++ server/config/prod.secret.exs
@@ -14,7 +14,7 @@ database_url =
 config :playhouse, Playhouse.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
