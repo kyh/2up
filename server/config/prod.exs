bedc0e66501c43c6a50fe24db5c681d85ff159b6
Andrew Lee <andyrewlee@gmail.com>
2020-02-05 23:32:28 -0800

    Check origin in socket

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :playhouse, PlayhouseWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["playhouse.gg"]
 
 # Do not print debug messages in production
 config :logger, level: :info
