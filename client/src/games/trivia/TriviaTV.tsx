4e6c2cdb8ebe5a92455393df16c295afdc5b65aa
Andrew Lee <andyrewlee@gmail.com>
2020-02-09 20:58:54 -0800

    Add scene 4

--- client/src/games/trivia/TriviaTV.tsx
+++ client/src/games/trivia/TriviaTV.tsx
@@ -3,6 +3,7 @@ import { TriviaContext } from 'games/trivia/TriviaContext';
 import { Scene1 } from './tv/Scene1';
 import { Scene2 } from './tv/Scene2';
 import { Scene3 } from './tv/Scene3';
+import { Scene4 } from './tv/Scene4';
 
 export const TriviaTV: React.FC = () => {
   const { state, broadcast } = useContext(TriviaContext);
@@ -14,6 +15,8 @@ export const TriviaTV: React.FC = () => {
       return <Scene2 broadcast={broadcast} state={state} />;
     case 3:
       return <Scene3 broadcast={broadcast} state={state} />;
+    case 4:
+      return <Scene4 broadcast={broadcast} state={state} />;
     default:
       return null;
   }
