e1649d9b592d098149c907ca0f16bdbeebb6ff09
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-09 16:47:25 -0800

    Finish styling intro

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
-import { PageContainer } from 'components';
-import { Flex, Button, Input } from 'components';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { TriviaPages } from './Trivia';
 
@@ -9,32 +7,9 @@ export const App: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <PageContainer>
-          <AppIntro />
-        </PageContainer>
+        <Redirect to="/trivia" />
       </Route>
       <TriviaPages />
     </Switch>
-  );
-};
-
-const AppIntro = () => {
-  const history = useHistory();
-  const [name, setName] = useState(localStorage.getItem('name') || '');
-
-  const onClickStart = () => {
-    localStorage.setItem('name', name);
-    history.push('/trivia');
-  };
-
-  return (
-    <Flex alignItems="center" flexDirection="column" mt={3}>
-      <Input
-        placeholder="Name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
-      <Button onClick={onClickStart}>Start</Button>
-    </Flex>
   );
 };
