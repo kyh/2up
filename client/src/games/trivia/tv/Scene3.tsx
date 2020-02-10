6fa1a56b7aabd4efe2327bb7fe77368e4f849997
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-09 21:17:04 -0800

    Finish scene 3 for TV

--- client/src/games/trivia/tv/Scene3.tsx
+++ client/src/games/trivia/tv/Scene3.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
-import styled from 'styled-components';
 import { SceneProps } from 'games/trivia/TriviaContext';
+import { Button } from 'components';
+import { hashCode } from 'utils/stringUtils';
 import { Question } from '../components/Question';
 import { SubmissionsContainer } from '../components/SubmissionsContainer';
 
 export const Scene3 = ({ state }: SceneProps) => {
   return (
-    <>
+    <section>
       <Question>{state.question}</Question>
       <SubmissionsContainer>
         {state.submissions.map(submission => {
           return (
-            <div key={submission.id}>
-              <div>
-                {submission.content} by {submission.name}
-              </div>
-              <div>
+            <div className="submission" key={submission.id}>
+              <Button disabled>{submission.content}</Button>
+              <div className="endorsement-container">
                 {submission.endorsers.map(endorser => {
-                  return <p key={endorser.id}>{endorser.name}</p>;
+                  const avatar = hashCode(endorser.name, 10);
+                  return (
+                    <div className="endorsement" key={endorser.id}>
+                      <img src={`/avatars/${avatar}.svg`} alt={endorser.name} />
+                      {endorser.name}
+                    </div>
+                  );
                 })}
               </div>
             </div>
           );
         })}
       </SubmissionsContainer>
-    </>
+    </section>
   );
 };
