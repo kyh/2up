f9759cfeebce3f94386205f6301d6211d8983bf4
Andrew Lee <andyrewlee@gmail.com>
2020-02-09 20:47:05 -0800

    Move remote scenes out

---
+++ client/src/games/trivia/remote/Scene1.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Flex, Input, Button } from 'components';
+import { SceneProps } from 'games/trivia/TriviaContext';
+
+export const Scene1 = ({ state, broadcast }: SceneProps) => {
+  const [value, setValue] = useState('');
+  const [errorValue, setErrorValue] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleClick = () => {
+    if (value.toLowerCase() === state?.answer?.toLowerCase()) {
+      setErrorValue('You have selected the right answer. Please write a tricky wrong answer instead.');
+      return;
+    }
+
+    broadcast('player:submit', {
+      name: localStorage.getItem('name'),
+      gameID: state.gameID,
+      submission: value
+    });
+
+    setSubmitted(true);
+  };
+
+  return (
+    <Flex alignItems="center" flexDirection="column">
+      <h1>{state.question}</h1>
+      <Input
+        value={value}
+        onChange={e => {
+        setErrorValue('')
+        setValue(e.target.value);
+        }}
+        readOnly={submitted}
+      />
+      <p>{errorValue}</p>
+      <Button disabled={submitted} onClick={handleClick}>
+        Submit answer
+      </Button>
+    </Flex>
+  );
+};
