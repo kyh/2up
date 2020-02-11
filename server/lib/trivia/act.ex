4789e5d5593f561134651d2928f868ee0878081c
Andrew Lee <andyrewlee@gmail.com>
2020-02-10 22:33:34 -0800

    Setup more game models

---
+++ server/lib/trivia/act.ex
@@ -0,0 +1,14 @@
+defmodule Trivia.Act do
+  defstruct question: '', answer: '', submissions: [], endorsements: []
+
+  alias Trivia.{Act}
+
+  def new(question, answer, submissions, endorsements) do
+    %Act{
+      question: question,
+      answer: answer,
+      submissions: submissions,
+      endorsements: endorsements
+    }
+  end
+end
--- server/lib/trivia/game.ex
+++ server/lib/trivia/game.ex
@@ -1,9 +1,19 @@
 defmodule Trivia.Game do
-  defstruct act: 0, scene: 0, questions: [], players: []
+  alias Trivia.{Act, Game, Player}
 
-  alias Trivia.{Game}
+  defstruct act: 0, scene: 0, acts: [], players: []
 
-  def new(questions, players) do
-    %Game{questions: questions, players: players}
+  def new(question_sets, player_ids) do
+    players = Enum.map player_ids, fn player_id ->
+      %Player{id: player_id}
+    end
+
+    acts = Enum.map question_sets, fn question_set ->
+      question = Enum.at(question_set, 0)
+      answer = Enum.at(question_set, 1)
+      %Act{ question: question, answer: answer }
+    end
+
+    %Game{acts: acts, players: players}
   end
 end
