8e01742b2a7a835301709fae4e5c3f2a0143d7a9
Andrew Lee <andyrewlee@gmail.com>
2020-02-10 21:38:09 -0800

    Remove trivia app

--- trivia/lib/trivia/game.ex
+++
@@ -1,9 +0,0 @@
-defmodule Trivia.Game do
-  defstruct act: 0, scene: 0, questions: [], players: []
-
-  alias Trivia.{Game}
-
-  def new(questions, players) do
-    %Game{questions: questions, players: players}
-  end
-end
