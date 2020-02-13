5de9e7ef5c8c4b8ed5c7d9751b51ad4d534623b5
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-13 01:19:57 -0800

    Fix bugs

---
+++ client/src/games/trivia/components/SubmissionCountBox.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const SubmissionCountBox = styled.div`
+  position: absolute;
+  top: ${({ theme }) => theme.spacings(3)};
+  color: ${({ theme }) => theme.colors.white};
+  background: ${({ theme }) => theme.colors.black};
+  padding: ${({ theme }) => theme.spacings(3)};
+  border-radius: 30px 2px 30% 3px / 4px 10px 3px 30px;
+`;
--- client/src/games/trivia/tv/Scene1.tsx
+++ client/src/games/trivia/tv/Scene1.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
 import { SceneProps } from 'games/trivia/TriviaContext';
 import { Question } from '../components/Question';
+import { SubmissionCountBox } from '../components/SubmissionCountBox';
 
 export const Scene1 = ({ state }: SceneProps) => {
   const submissions = state.submissions.length - 1;
   return (
     <>
       {!!submissions && (
-        <SubmissionBox>
+        <SubmissionCountBox>
           {submissions} players have submitted their answers
-        </SubmissionBox>
+        </SubmissionCountBox>
       )}
       <Question>{state.question}</Question>
     </>
   );
 };
-
-const SubmissionBox = styled.div`
-  position: absolute;
-  top: ${({ theme }) => theme.spacings(3)};
-  color: ${({ theme }) => theme.colors.white};
-  background: ${({ theme }) => theme.colors.black};
-  padding: ${({ theme }) => theme.spacings(3)};
-  border-radius: 30px 2px 30% 3px / 4px 10px 3px 30px;
-`;
