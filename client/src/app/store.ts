e59d739c4f75828b778fc9c9a6cd28be96815118
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-19 01:03:13 -0800

    Add logger

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,21 @@
-import { configureStore, Action } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware, Action } from '@reduxjs/toolkit';
 import { ThunkAction } from 'redux-thunk';
-
+import { createLogger } from 'redux-logger';
 import rootReducer, { RootState } from './rootReducer';
 
+const logger = createLogger({
+  predicate: (_getState, action) => action.type.includes('trivia')
+});
+
+const middleware = [...getDefaultMiddleware()];
+
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
+
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware
 });
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
