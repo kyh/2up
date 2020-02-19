1bcf113b3690825e00e47142d639e35300a84e3a
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-19 02:46:18 -0800

    Update types

--- client/src/features/trivia/triviaSlice.ts
+++ client/src/features/trivia/triviaSlice.ts
@@ -1,26 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { GameState, Player } from 'features/types';
 
-export interface TriviaGameState extends GameState {
+export type TriviaState = {
   act: number;
   scene: number;
   question?: string;
   answer?: string;
   players: Player[];
   submissions: Submission[];
-}
+};
 
-export interface Submission {
+export type TriviaGameState = TriviaState & GameState;
+
+export type Submission = {
   id: number;
   name: string;
   content: string;
   endorsers: Player[];
-}
+};
 
-export interface SceneProps {
+export type SceneProps = {
   state: TriviaGameState;
   broadcast: (_eventName: string, _payload?: object) => void;
-}
+};
 
 export const initialState: TriviaGameState = {
   gameID: '',
@@ -36,10 +38,10 @@ const triviaSlice = createSlice({
   name: 'trivia',
   initialState,
   reducers: {
-    new_game(state, { payload }) {
+    new_game(state, { payload }: PayloadAction<{ gameID: string }>) {
       state.gameID = payload.gameID;
     },
-    game_state(state, { payload }) {
+    game_state(state, { payload }: PayloadAction<TriviaState>) {
       state.players = payload.players ?? state.players;
       state.act = payload.act ?? state.act;
       state.scene = payload.scene ?? state.scene;
