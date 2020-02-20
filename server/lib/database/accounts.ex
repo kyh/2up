6da1477201bcef99b50c05462e1ae56cc785a918
Andrew Lee <andyrewlee@gmail.com>
2020-02-19 22:16:16 -0800

    Create users and accounts context

---
+++ server/lib/database/accounts.ex
@@ -0,0 +1,26 @@
+defmodule Database.Accounts do
+  import Ecto.Query, warn: false
+  alias Database.Repo
+  alias Database.Accounts.User
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def create_user(attrs) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def authenticate(username, password) do
+    user = Repo.get_by(User, username: username)
+
+    with %{password_hash: password_hash} <- user,
+         true <- Pbkdf2.verify_pass(password, password_hash) do
+      {:ok, user}
+    else
+      _ -> :error
+    end
+  end
+end
