d5910d1e29f16d361f4846f7c99a2305e24b3b08
Andrew Lee <andyrewlee@gmail.com>
2020-02-19 23:08:32 -0800

    Signin and singup mutations

--- server/lib/web/graphql/schema.ex
+++ server/lib/web/graphql/schema.ex
@@ -1,6 +1,6 @@
 defmodule Web.GraphQL.Schema do
   use Absinthe.Schema
-  alias Web.GraphQL.Resolvers.{Catalog}
+  alias Web.GraphQL.Resolvers.{Catalog, Accounts}
 
   query do
     @desc "Get a list of questions"
@@ -9,8 +9,35 @@ defmodule Web.GraphQL.Schema do
     end
   end
 
+  mutation do
+    @desc "Create user"
+    field :signup, :session do
+      arg :username, non_null(:string)
+      arg :email, non_null(:string)
+      arg :password, non_null(:string)
+      resolve &Accounts.signup/3
+    end
+
+    @desc "Sign in user"
+    field :signin, :session do
+      arg :username, non_null(:string)
+      arg :password, non_null(:string)
+      resolve &Accounts.signin/3
+    end
+  end
+
   object :question do
     field :id, non_null(:id)
     field :content, non_null(:string)
+  end
+
+  object :user do
+    field :username, non_null(:string)
+    field :email, non_null(:string)
+  end
+
+  object :session do
+    field :user, non_null(:user)
+    field :token, non_null(:string)
   end
 end
