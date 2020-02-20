61e7b130cdc5f4732fdd11a608805d890a2eddcc
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-19 21:21:10 -0800

    Add a middleware to whitelist certain action types

---
+++ client/src/utils/socketMiddleware.ts
@@ -0,0 +1,10 @@
+const WHITELIST_EVENTS = ['playhouse', 'trivia'].join('|');
+
+export const socketMiddleware = (_store: any) => (next: any) => (
+  action: any
+) => {
+  const reg = new RegExp(WHITELIST_EVENTS, 'gi');
+  if (reg.test(action.type)) {
+    next(action);
+  }
+};
