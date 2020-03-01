b766bc8867cae7e694cd3a27171acbb474e4475e
Andrew Lee <andyrewlee@gmail.com>
2020-02-29 18:21:08 -0800

    Specify pack from frontend

--- server/lib/trivia/question_cache.ex
+++ server/lib/trivia/question_cache.ex
@@ -7,8 +7,8 @@ defmodule Trivia.QuestionCache do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  def get_questions() do
-    GenServer.call(__MODULE__, :get_questions)
+  def get_questions(pack) do
+    GenServer.call(__MODULE__, {:get_questions, pack})
   end
 
   def init(:ok) do
@@ -17,8 +17,13 @@ defmodule Trivia.QuestionCache do
     {:ok, state}
   end
 
-  def handle_call(:get_questions, _from, state) do
-    {:reply, state, state}
+  def handle_call({:get_questions, pack}, _from, state) do
+    questions = Enum.filter(state, fn x ->
+      pack_field = Enum.at(x, 2)
+      pack_name = Enum.at(pack_field, 0)
+      pack_name == pack
+    end)
+    {:reply, questions, state}
   end
 
   def handle_info(:refresh, _state) do
@@ -31,7 +36,7 @@ defmodule Trivia.QuestionCache do
     Process.send_after(self(), :refresh, @refresh_interval)    
   end
 
-  def load_questions() do
+  defp load_questions() do
     key = System.get_env("AIRTABLE_KEY")
     url = "https://api.airtable.com/v0/appOUKhim5DD45JMb/Question%20Bank\?api_key\=#{key}"
     response = HTTPoison.get!(url)
