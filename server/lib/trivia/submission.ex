11f136e14ac3748bc9c3d5f01379a291725b7fd4
Andrew Lee <andyrewlee@gmail.com>
2020-02-29 20:13:12 -0800

    Remove unused files

--- server/lib/trivia/submission.ex
+++
@@ -1,9 +0,0 @@
-defmodule Trivia.Submission do
-  defstruct content: '', player_id: 0
-
-  alias Trivia.{Submission}
-
-  def new(content, player_id) do
-    %Submission{content: content, player_id: player_id}
-  end
-end
