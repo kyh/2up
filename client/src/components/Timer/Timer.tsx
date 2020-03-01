0d3fccabd2d23db7a25b1cd8e109adbbea203d71
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-02-29 19:47:46 -0800

    Add snail

--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import styled, { keyframes } from 'styled-components';
+import raw from 'raw.macro';
 
 type Props = {
   initialSeconds: number;
   onTimeout?: () => void;
 };
+
+const snail = raw('./snail.svg');
 
 export const Timer: React.FC<Props> = ({
   initialSeconds,
@@ -23,5 +28,51 @@ export const Timer: React.FC<Props> = ({
     return () => clearInterval(interval);
   }, [seconds]);
 
-  return <div>{seconds} seconds remaining</div>;
+  return ReactDOM.createPortal(
+    <TimerContainer initialSeconds={initialSeconds}>
+      <Snail dangerouslySetInnerHTML={{ __html: snail }} />
+      <div>{seconds} seconds remaining</div>
+    </TimerContainer>,
+    document.body
+  );
 };
+
+const scaleAnimation = keyframes`
+  0% { transform: scaleX(1); }
+  50% { transform: scaleX(0.95); }
+  100% { transform: scaleX(1); }
+`;
+
+const eyeAnimation = keyframes`
+  0% { transform: translate(0); }
+  50% { transform: translate(3px, 0); }
+  100% { transform: translate(0); }
+`;
+
+const moveAnimation = keyframes`
+  from { transform: translate(100vw) }
+  to { transform: translate(0vw) }
+`;
+
+const Snail = styled.div`
+  .right-eye {
+    animation: ${eyeAnimation} 1s ease infinite;
+    animation-delay: 0.1s;
+  }
+  .body {
+    animation: ${scaleAnimation} 1s ease infinite;
+    animation-delay: 0.1s;
+  }
+  .shell {
+    animation: ${scaleAnimation} 1s ease infinite;
+  }
+`;
+
+const TimerContainer = styled.div<{ initialSeconds: number }>`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  animation: ${moveAnimation} ${({ initialSeconds }) => initialSeconds}s linear
+    infinite;
+  animation-iteration-count: 1;
+`;
