23a3d032c80549ead96cc9ebb89c6f25991e892c
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-08 21:22:13 -0700

    Remove packs and add instruction

--- client/src/features/trivia/triviaSlice.ts
+++ client/src/features/trivia/triviaSlice.ts
@@ -7,6 +7,7 @@ export type TriviaState = {
   act: number;
   scene: number;
   question?: string;
+  instruction?: string;
   answer?: string;
   players: Player[];
   submissions: Submission[];
@@ -28,12 +29,12 @@ export type SceneProps = {
 
 export const initialState: TriviaGameState = {
   isHost: localStorage.getItem('isHost') === 'true',
-  packs: [],
   gameID: '',
   players: [],
   act: 0,
   scene: 0,
   question: '',
+  instruction: '',
   answer: '',
   submissions: []
 };
@@ -45,14 +46,12 @@ const triviaSlice = createSlice({
     new_game(state, { payload }: PayloadAction<{ gameID: string }>) {
       state.gameID = payload.gameID;
     },
-    packs(state, { payload }: PayloadAction<string[]>) {
-      state.packs = Object.keys(payload);
-    },
     game_state(state, { payload }: PayloadAction<TriviaState>) {
       state.players = payload.players ?? state.players;
       state.act = payload.act ?? state.act;
       state.scene = payload.scene ?? state.scene;
       state.question = payload.question ?? state.question;
+      state.instruction = payload.instruction ?? state.instruction;
       state.answer = payload.answer ?? state.answer;
       state.submissions = payload.submissions ?? state.submissions;
     },
