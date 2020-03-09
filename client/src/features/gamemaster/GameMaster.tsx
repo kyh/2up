2e6ccf735678e464cd5fa3c20fc9dbc48704c095
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-08 23:50:25 -0700

    Fix gamemaster view

--- client/src/features/gamemaster/GameMaster.tsx
+++ client/src/features/gamemaster/GameMaster.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 
 const GET_QUESTIONS = gql`
-  mutation GetQuestions {
+  query GetQuestions {
     questions {
       id
       content
@@ -12,15 +13,21 @@ const GET_QUESTIONS = gql`
 `;
 
 export const GameMaster = () => {
-  const { data } = useQuery(GET_QUESTIONS);
+  const { data, loading } = useQuery(GET_QUESTIONS);
+  if (loading) return null;
   return (
-    <>
+    <Page>
       <h1>Game Master view</h1>
       <div>
         {data.questions.map((question: any) => (
           <div key={question.id}>{question.content}</div>
         ))}
       </div>
-    </>
+    </Page>
   );
 };
+
+const Page = styled.section`
+  overflow: auto;
+  height: calc((var(--vh, 1vh) * 100) - 50px);
+`;
