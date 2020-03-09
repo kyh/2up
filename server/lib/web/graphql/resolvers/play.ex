ec14421ad256accda0c4937c0f62544e43c3d9db
Andrew Lee <andyrewlee@gmail.com>
2020-03-08 21:38:19 -0700

    Add game field to check validity

--- server/lib/web/graphql/resolvers/play.ex
+++ server/lib/web/graphql/resolvers/play.ex
@@ -1,6 +1,7 @@
 defmodule Web.GraphQL.Resolvers.Play do
   alias Database.Catalog
   alias Trivia.GameSupervisor
+  alias Trivia.GameServer
 
   def trivia_new(_, _, _) do
     code = Catalog.generate_code()
@@ -10,6 +11,15 @@ defmodule Web.GraphQL.Resolvers.Play do
         {:ok, %{code: code}}
       {:error, _error} ->
         {:error, message: "Server error"}
+    end
+  end
+
+  def game_validate(_, %{code: code}, _) do
+    case GameServer.game_pid(code) do
+      pid when is_pid(pid) ->
+        {:ok, %{is_valid: true}}
+      nil ->
+        {:ok, %{is_valid: false}}
     end
   end
 end
