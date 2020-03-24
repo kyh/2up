ead20b203de92945067f62c9ac1575436a3fb4ef
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-23 21:31:56 -0700

    Add modal prompt

--- client/src/features/trivia/TriviaLobby.tsx
+++ client/src/features/trivia/TriviaLobby.tsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
-import { Button } from 'components';
+import { Button, Modal } from 'components';
 import { hashCode } from 'utils/stringUtils';
 import { useTriviaChannel } from 'features/trivia/TriviaChannel';
 
 export const TriviaLobby = () => {
   const history = useHistory();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { state: triviaState, broadcast } = useTriviaChannel();
 
   const onClickStart = () => {
+    if (triviaState.players.length < 2) {
+      setIsModalOpen(true);
+    } else {
+      onStart();
+    }
+  };
+
+  const onStart = () => {
     broadcast('start', { gameID: triviaState.gameID });
   };
 
@@ -26,15 +35,13 @@ export const TriviaLobby = () => {
   return (
     <LobbyContainer>
       {triviaState.isHost ? (
-        <>
-          <div className="title-container">
-            <h1 className="title">
-              Go to <span className="highlight">playhouse.gg</span>
-            </h1>
-            <h1 className="title">and enter the room code:</h1>
-          </div>
+        <TitleContainer>
+          <h1 className="title">
+            Go to <span className="highlight">playhouse.gg</span>
+          </h1>
+          <h1 className="title">and enter the room code:</h1>
           <div className="game-id">{triviaState.gameID}</div>
-        </>
+        </TitleContainer>
       ) : (
         <h1 className="title">Waiting for players to join...</h1>
       )}
@@ -54,6 +61,30 @@ export const TriviaLobby = () => {
           <Button className="start-game-button" onClick={onClickStart}>
             Start game
           </Button>
+          <Modal
+            open={isModalOpen}
+            title="Are you sure?"
+            onRequestClose={() => setIsModalOpen(false)}
+            maxWidth={300}
+            closeButton
+          >
+            <StartModalBody>
+              <p>
+                This game is only fun with 2 or more players. Invite more by
+                sending them to:
+              </p>
+              <TitleContainer>
+                <h3 className="title">
+                  <span className="highlight">playhouse.gg</span>
+                </h3>
+                <h3 className="title">and enter the room code:</h3>
+                <div className="game-id">{triviaState.gameID}</div>
+              </TitleContainer>
+              <Button className="start-game-button" onClick={onStart}>
+                Start anyways
+              </Button>
+            </StartModalBody>
+          </Modal>
         </>
       ) : (
         <Link className="join-button" to="/">
@@ -68,11 +99,20 @@ const LobbyContainer = styled.section`
   height: 100%;
   display: flex;
   flex-direction: column;
-
-  .title-container {
-    text-align: center;
-    margin-top: ${({ theme }) => theme.spacings(2)};
+  .start-game-button {
+    margin: ${({ theme }) => theme.spacings(4)} auto;
   }
+  .join-button {
+    display: block;
+    text-align: center;
+    text-decoration: underline;
+    margin-top: auto;
+  }
+`;
+const TitleContainer = styled.div`
+  text-align: center;
+  margin-top: ${({ theme }) => theme.spacings(2)};
+
   .title {
     margin: 0 0 ${({ theme }) => theme.spacings(2)};
   }
@@ -89,15 +129,6 @@ const LobbyContainer = styled.section`
     padding: 20px 30px 30px;
     line-height: 1;
     border-radius: 19px 22px 30% 16px / 19px 17px 14px 30px;
-  }
-  .start-game-button {
-    margin: ${({ theme }) => theme.spacings(4)} auto;
-  }
-  .join-button {
-    display: block;
-    text-align: center;
-    text-decoration: underline;
-    margin-top: auto;
   }
 `;
 
@@ -143,4 +174,17 @@ const renderRemoteStyles = () => {
 };
 const LobbyPlayersContainer = styled.section<LobbyPlayersContainerProps>`
   ${({ isHost }) => (isHost ? renderTvStyles() : renderRemoteStyles())}
+`;
+const StartModalBody = styled.div`
+  p {
+    margin: 0 0 ${({ theme }) => theme.spacings(3)};
+  }
+  .game-id {
+    font-size: 2rem;
+    padding: ${({ theme }) => theme.spacings(5)};
+  }
+  .start-game-button {
+    width: 100%;
+    margin-top: ${({ theme }) => theme.spacings(2)};
+  }
 `;
