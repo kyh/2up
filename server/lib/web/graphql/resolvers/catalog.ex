2578b9dfb1c398bf022750ddfe30fc366f40ea7e
Andrew Lee <andyrewlee@gmail.com>
2020-04-01 18:10:37 -0700

    Add queries for packs and types

--- server/lib/web/graphql/resolvers/catalog.ex
+++ server/lib/web/graphql/resolvers/catalog.ex
@@ -4,4 +4,16 @@ defmodule Web.GraphQL.Resolvers.Catalog do
   def questions(_, _, _) do
     {:ok, Catalog.list_questions()}
   end
+
+  def question_types(_, _, _) do
+    {:ok, ["TEXT", "COLOR", "MAP", "BUTTON", "MATH"]}
+  end
+
+  def answer_types(_, _, _) do
+    {:ok, ["TEXT", "COLOR", "MAP", "BUTTON", "MATH"]}
+  end
+
+  def packs(_, _, _) do
+    {:ok, ["Startups", "SAT", "Variety"]}
+  end
 end
--- server/lib/web/graphql/schema.ex
+++ server/lib/web/graphql/schema.ex
@@ -14,6 +14,18 @@ defmodule Web.GraphQL.Schema do
     field :questions, list_of(:question) do
       resolve &Catalog.questions/3
     end
+
+    field :question_types, list_of(:string) do
+      resolve &Catalog.question_types/3
+    end
+
+    field :answer_types, list_of(:string) do
+      resolve &Catalog.answer_types/3
+    end
+
+    field :packs, list_of(:string) do
+      resolve &Catalog.packs/3
+    end
   end
 
   mutation do
