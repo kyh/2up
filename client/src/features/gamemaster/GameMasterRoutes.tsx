677a9408b3f6b2bf32cc67b6623a1a082263462b
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-04-12 15:23:35 -0700

    Update gamemaster

--- client/src/features/gamemaster/GameMasterRoutes.tsx
+++ client/src/features/gamemaster/GameMasterRoutes.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { PageContainer } from "components";
 
-import { GameMaster } from "features/gamemaster/GameMaster";
+import { ActPage } from "./ActPage";
 
 export const GameMasterRoutes = () => {
   return (
     <>
       <Route exact path="/gamemaster">
-        <PageContainer size="large" align="center">
-          <GameMaster />
-        </PageContainer>
+        <ActPage />
       </Route>
     </>
   );
