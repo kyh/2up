f0cd8acc6efd8395aa503acbf024d9f0a2bee88f
Andrew Lee <andyrewlee@gmail.com>
2020-04-26 11:20:55 -0700

    Remove schema generation from build step

--- server/build.sh
+++ server/build.sh
@@ -9,8 +9,5 @@ MIX_ENV=prod mix compile
 # Run migrations
 mix ecto.migrate
 
-# Generate schema.json and move to client
-mix absinthe.schema.json --schema Web.GraphQL.Schema --pretty ../client/schema.json
-
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
