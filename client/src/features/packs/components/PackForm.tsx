f04b1b8265b140bc9dfe900de2846857705a1dbf
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-10-11 20:58:26 -0700

    update packages

--- client/src/features/packs/components/PackForm.tsx
+++ client/src/features/packs/components/PackForm.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
-
 import { Button, TextField, AreaField } from "components";
 
 export type PackInputs = {
