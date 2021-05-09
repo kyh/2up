2df02234e5e16e8db16c7ea0624c46565c51ca32
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-05-08 18:39:41 -0700

    clean up submission code

--- client/src/features/game/steps/Step2.tsx
+++ client/src/features/game/steps/Step2.tsx
@@ -1,48 +1,50 @@
 import { AvatarImage, Button } from "components";
-import { StepProps } from "features/game/gameSlice";
+import { StepProps, GameState } from "features/game/gameSlice";
 import { hashCode } from "utils/stringUtils";
 import { SubmissionsContainer } from "features/game/components/SubmissionsContainer";
 import { Answer } from "features/game/components/Answer";
 import correctSvg from "features/game/components/correct.svg";
 
+const Submissions = ({ gameState }: { gameState: GameState }) => {
+  return (
+    <SubmissionsContainer>
+      {gameState.sceneAnswers?.map((sceneAnswer) => {
+        return (
+          <div className="submission full" key={sceneAnswer.id}>
+            {sceneAnswer.isCorrect && (
+              <img className="correct" src={correctSvg} alt="Correct answer" />
+            )}
+            <Answer
+              answerType={gameState.answerType}
+              sceneAnswer={sceneAnswer}
+              onSubmit={() => {}}
+              submitted
+            />
+            <div className="endorsement-container">
+              {gameState.submissions.map((submission) => {
+                const avatar = hashCode(submission.name, 10);
+                const show = submission.content === sceneAnswer.content;
+                if (!show) return null;
+                return (
+                  <div className="endorsement" key={submission.id}>
+                    <AvatarImage avatar={avatar} />
+                    {submission.name}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </SubmissionsContainer>
+  );
+};
+
 export const Step2 = ({ state, broadcast, name }: StepProps) => {
   const [firstPlayer] = state.players;
   return (
     <section>
-      <SubmissionsContainer>
-        {state.sceneAnswers?.map((sceneAnswer) => {
-          return (
-            <div className="submission full" key={sceneAnswer.id}>
-              {sceneAnswer.isCorrect && (
-                <img
-                  className="correct"
-                  src={correctSvg}
-                  alt="Correct answer"
-                />
-              )}
-              <Answer
-                answerType={state.answerType}
-                sceneAnswer={sceneAnswer}
-                onSubmit={() => {}}
-                submitted
-              />
-              <div className="endorsement-container">
-                {state.submissions.map((submission) => {
-                  const avatar = hashCode(submission.name, 10);
-                  const show = submission.content === sceneAnswer.content;
-                  if (!show) return null;
-                  return (
-                    <div className="endorsement" key={submission.id}>
-                      <AvatarImage avatar={avatar} />
-                      {submission.name}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </SubmissionsContainer>
+      <Submissions gameState={state} />
       {firstPlayer && (
         <Button
           disabled={firstPlayer.name !== name}
@@ -60,40 +62,7 @@ export const Step2 = ({ state, broadcast, name }: StepProps) => {
 export const Step2Spectate = ({ state }: StepProps) => {
   return (
     <section>
-      <SubmissionsContainer>
-        {state.sceneAnswers?.map((sceneAnswer) => {
-          return (
-            <div className="submission full" key={sceneAnswer.id}>
-              {sceneAnswer.isCorrect && (
-                <img
-                  className="correct"
-                  src={correctSvg}
-                  alt="Correct answer"
-                />
-              )}
-              <Answer
-                answerType={state.answerType}
-                sceneAnswer={sceneAnswer}
-                onSubmit={() => {}}
-                submitted
-              />
-              <div className="endorsement-container">
-                {state.submissions.map((submission) => {
-                  const avatar = hashCode(submission.name, 10);
-                  const show = submission.content === sceneAnswer.content;
-                  if (!show) return null;
-                  return (
-                    <div className="endorsement" key={submission.id}>
-                      <AvatarImage avatar={avatar} />
-                      {submission.name}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </SubmissionsContainer>
+      <Submissions gameState={state} />
     </section>
   );
 };
