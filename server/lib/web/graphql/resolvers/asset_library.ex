d728afda73b7bb1a61fff14ecff0d939457af9d4
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-06-23 01:10:39 -0700

    update bucket name

--- server/lib/web/graphql/resolvers/asset_library.ex
+++ server/lib/web/graphql/resolvers/asset_library.ex
@@ -2,13 +2,13 @@ defmodule Web.GraphQL.Resolvers.AssetLibrary do
   def presigned_url(_, %{path: path}, _) do
     {:ok, presigned_url} =
       ExAws.Config.new(:s3)
-      |> ExAws.S3.presigned_url(:put, "playhouse.gg", path)
+      |> ExAws.S3.presigned_url(:put, "assets.playhouse.gg", path)
 
     {:ok, %{presigned_url: presigned_url}}
   end
 
   def list(_, %{path: path}, _) do
-    # ExAws.S3.list_objects("playhouse.gg", path)
+    # ExAws.S3.list_objects("assets.playhouse.gg", path)
     # |> ExAws.request!()
     # |> get_in([:body, :contents])
   end
