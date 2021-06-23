f4a1c9299297568349a402512dcd4b4fc3d8bda8
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-06-23 06:01:32 -0700

    generate random filename for upload

--- client/src/utils/stringUtils.ts
+++ client/src/utils/stringUtils.ts
@@ -39,3 +39,12 @@ const isNil = (value: any) => {
 const isObject = (obj = {}) => {
   return Object.prototype.toString.call(obj) === "[object Object]";
 };
+
+export const uuid = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    // eslint-disable-next-line no-mixed-operators
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
