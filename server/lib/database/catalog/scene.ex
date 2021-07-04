a15311c0b8c4f9abc04010b64c9968b833adc7f0
Andrew Lee <andyrewlee@gmail.com>
2021-07-03 22:34:22 -0700

    Use string in schema

--- server/lib/database/catalog/scene.ex
+++ server/lib/database/catalog/scene.ex
@@ -8,7 +8,7 @@ defmodule Database.Catalog.Scene do
     belongs_to :question_type, QuestionType, on_replace: :update
     belongs_to :answer_type, AnswerType, on_replace: :update
 
-    field :question, :text
+    field :question, :string
     field :instruction, :string
     field :order, :decimal
     field :external_id, :string
