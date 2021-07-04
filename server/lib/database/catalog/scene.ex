b9238a9ad7d0804ca76140e7f34447d7bfbd1f80
Andrew Lee <andyrewlee@gmail.com>
2021-07-03 22:18:29 -0700

    Change question from string to text

--- server/lib/database/catalog/scene.ex
+++ server/lib/database/catalog/scene.ex
@@ -8,7 +8,7 @@ defmodule Database.Catalog.Scene do
     belongs_to :question_type, QuestionType, on_replace: :update
     belongs_to :answer_type, AnswerType, on_replace: :update
 
-    field :question, :string
+    field :question, :text
     field :instruction, :string
     field :order, :decimal
     field :external_id, :string
