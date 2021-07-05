5d15f42fe2ff083b83429b7933f7d926e236a613
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-07-05 00:03:56 -0700

    add beta signup page

--- client/src/features/auth/AuthRoutes.tsx
+++ client/src/features/auth/AuthRoutes.tsx
@@ -1,9 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import { AuthPage } from "features/auth/AuthPage";
+import { RequestPage } from "features/auth/RequestPage";
 
 export const AuthRoutes = () => {
   return (
     <Switch>
+      <Route path="/auth/request">
+        <RequestPage />
+      </Route>
       <Route path="/auth/signup">
         <AuthPage />
       </Route>
