3e7e479c3dd99bd26903d9d0be6b8048fb9698b8
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-08-01 19:14:13 -0700

    update favicons

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -46,24 +46,27 @@ export default class MyDocument extends Document {
           <meta name="robots" content="index, follow" />
           <meta property="fb:app_id" content="{FB_ID}" />
 
-          <link rel="manifest" href="/manifest.json" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
+            href="/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon/favicon-32x32.png"
+            href="/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon/favicon-16x16.png"
+            href="/favicon-16x16.png"
           />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
