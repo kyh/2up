c3e2589b376e98db2b6f3ca0f913b1ba0818533f
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-09-05 16:31:27 -0700

    remove redux

--- web/src/utils/string.ts
+++ web/src/utils/string.ts
@@ -1,5 +1,4 @@
 import memoize from "lodash/memoize";
-import omit from "lodash/omit";
 
 const hashCodeFn = (string?: string, mod?: number) => {
   let hash = 0;
@@ -14,40 +13,3 @@ const hashCodeFn = (string?: string, mod?: number) => {
 };
 
 export const hashCode = memoize(hashCodeFn);
-
-type OmitInput = Record<string, any>;
-
-export const omitDeep = (input: OmitInput, ...props: string[]) => {
-  const omitDeepOnOwnProps = (obj: OmitInput): OmitInput => {
-    if (typeof input === "undefined") return input;
-    if (!Array.isArray(obj) && !isObject(obj)) return obj;
-    if (Array.isArray(obj)) return omitDeep(obj, ...props);
-
-    const o: OmitInput = {};
-    for (const [key, value] of Object.entries(obj)) {
-      o[key] = !isNil(value) ? omitDeep(value, ...props) : value;
-    }
-
-    return omit(o, props);
-  };
-
-  if (Array.isArray(input)) return input.map(omitDeepOnOwnProps);
-  return omitDeepOnOwnProps(input);
-};
-
-const isNil = (value: unknown) => {
-  return value === null || value === undefined;
-};
-
-const isObject = (obj: object) => {
-  return Object.prototype.toString.call(obj) === "[object Object]";
-};
-
-export const uuid = () => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === "x" ? r : (r & 0x3) | 0x8;
-
-    return v.toString(16);
-  });
-};
