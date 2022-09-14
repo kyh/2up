e55e678362100c5dac38932271859434df34c224
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-09-13 21:52:59 -0700

    finish game

--- web/src/lib/game/gameUtils.ts
+++ web/src/lib/game/gameUtils.ts
@@ -60,7 +60,29 @@ export const AnswerTypeSlugs = {
   // },
 };
 
-export const maxScorePerScene = 200;
+const scoreForCorrectAnswer = 100;
+const maxBonusScore = 100;
+
+export const maxScorePerScene = scoreForCorrectAnswer + maxBonusScore;
+
+const clamp = (a: number, min = 0, max = 1) => Math.min(max, Math.max(min, a));
+const invlerp = (x: number, y: number, a: number) => clamp((a - x) / (y - x));
+
+export const calculateScore = (
+  isCorrect: boolean,
+  startTime: number,
+  endTime: number,
+  duration: number
+) => {
+  if (!isCorrect) return 0;
+
+  const difference = Math.round((endTime - startTime) / 1000);
+  if (difference > duration) return scoreForCorrectAnswer;
+
+  const timeScore = (1 - invlerp(0, duration, difference)) * maxBonusScore;
+
+  return scoreForCorrectAnswer + timeScore;
+};
 
 export const compareAnswer = (answer: string, answerToCompare: string) => {
   return answer.trim().toLowerCase() === answerToCompare.trim().toLowerCase();
--- web/src/server/router/game.ts
+++ web/src/server/router/game.ts
@@ -2,7 +2,7 @@ import type { Scene, PrismaClient } from "@prisma/client";
 import { t, ServerError } from "~/server/trpc";
 import { z } from "zod";
 import { customAlphabet } from "nanoid";
-import { maxScorePerScene, compareAnswer, upsert } from "~/lib/game/gameUtils";
+import { compareAnswer, upsert, calculateScore } from "~/lib/game/gameUtils";
 import type { GameState } from "~/lib/game/gameStore";
 
 const nanoid = customAlphabet("1234567890", 5);
@@ -70,13 +70,13 @@ export const gameRouter = t.router({
       });
 
       const gameState: GameState = {
-        currentScene: 0,
         currentStep: 1,
-        submissions: [],
+        currentScene: 0,
         totalScenes: scenes.length,
+        playerScores: [],
+        submissions: [],
         duration: 45,
         startTime: Date.now(),
-        playerScores: [],
         questionDescription: firstScene.questionDescription,
         question: firstScene.question,
         questionType: firstScene.questionType,
@@ -113,19 +113,79 @@ export const gameRouter = t.router({
   start: t.procedure
     .input(z.object({ gameId: z.string() }))
     .mutation(async ({ ctx, input }) => {
+      const game = await findGameOrThrow(ctx.prisma, input.gameId);
+      const gameState = game.state as unknown as GameState;
+
       return ctx.prisma.game.update({
         where: { id: input.gameId },
-        data: { isStarted: true },
+        data: {
+          isStarted: true,
+          state: {
+            ...gameState,
+            startTime: Date.now(),
+          },
+        },
       });
     }),
   nextStep: t.procedure
     .input(z.object({ gameId: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      // TODO
       const game = await findGameOrThrow(ctx.prisma, input.gameId);
       const gameState = game.state as unknown as GameState;
 
-      return game;
+      return ctx.prisma.game.update({
+        where: { id: input.gameId },
+        data: {
+          state: {
+            ...gameState,
+            currentStep: gameState.currentStep + 1,
+          },
+        },
+      });
+    }),
+  nextScene: t.procedure
+    .input(z.object({ gameId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const game = await findGameOrThrow(ctx.prisma, input.gameId);
+      const gameState = game.state as unknown as GameState;
+
+      const nextScene = gameState.currentScene + 1;
+      const nextSceneId = game.gameScenes[nextScene];
+
+      if (!nextSceneId) {
+        return ctx.prisma.game.update({
+          where: { id: input.gameId },
+          data: { state: { ...gameState, currentStep: 0 } },
+        });
+      }
+
+      const scene = await ctx.prisma.scene.findUnique({
+        where: { id: nextSceneId },
+        include: { answers: true },
+      });
+
+      return ctx.prisma.game.update({
+        where: { id: input.gameId },
+        data: {
+          state: {
+            ...gameState,
+            currentStep: 1,
+            currentScene: nextScene,
+            submissions: [],
+            duration: 45,
+            startTime: Date.now(),
+            questionDescription: scene?.questionDescription,
+            question: scene?.question,
+            questionType: scene?.questionType,
+            answerType: scene?.answerType,
+            sceneAnswers: scene?.answers.map((sa) => ({
+              id: sa.id,
+              content: sa.content,
+              isCorrect: sa.isCorrect,
+            })),
+          },
+        },
+      });
     }),
   submitAnswer: t.procedure
     .input(
@@ -159,7 +219,12 @@ export const gameRouter = t.router({
       const playerScore = {
         playerId: input.submission.playerId,
         playerName: input.submission.playerName,
-        score: 0,
+        score: calculateScore(
+          submission.isCorrect,
+          gameState.startTime,
+          Date.now(),
+          gameState.duration
+        ),
         prevScore: 0,
       };
 
