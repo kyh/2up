4ec012d209f9386fef2e71a4d0d8270abcf75fa9
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-09-24 09:20:41 -0700

    start crud for packs

--- web/src/lib/packs/usePackActions.ts
+++ web/src/lib/packs/usePackActions.ts
@@ -1,18 +1,6 @@
 import { trpc } from "~/utils/trpc";
 import { useAlert } from "~/components";
 
-export const useGetPack = (packId: string) => {
-  const alert = useAlert();
-  return trpc.pack.getPack.useQuery(
-    { packId },
-    {
-      onError: (err) => {
-        alert.show(err.message);
-      },
-    }
-  );
-};
-
 export const useGetDiscover = (ref: string) => {
   const alert = useAlert();
   return trpc.pack.getDiscover.useQuery(
@@ -32,4 +20,73 @@ export const useGetPacks = (filter: Record<string, unknown>) => {
       alert.show(err.message);
     },
   });
+};
+
+export const useGetPack = (packId: string) => {
+  const alert = useAlert();
+  return trpc.pack.get.useQuery(
+    { packId },
+    {
+      onError: (err) => {
+        alert.show(err.message);
+      },
+    }
+  );
+};
+
+export const useCreatePack = () => {
+  const alert = useAlert();
+  const mutation = trpc.pack.create.useMutation();
+
+  const createPack = async (pack: Parameters<typeof mutation.mutate>[0]) => {
+    await mutation.mutate(pack, {
+      onError: (err) => {
+        alert.show(err.message);
+      },
+    });
+  };
+
+  return { ...mutation, createPack };
+};
+
+export const useUpdatePack = () => {
+  const alert = useAlert();
+  const mutation = trpc.pack.update.useMutation();
+
+  const updatePack = async (
+    packId: string,
+    pack: Parameters<typeof mutation.mutate>[0]
+  ) => {
+    await mutation.mutate(
+      {
+        ...pack,
+        packId,
+      },
+      {
+        onError: (err) => {
+          alert.show(err.message);
+        },
+      }
+    );
+  };
+
+  return { ...mutation, updatePack };
+};
+
+export const useDeletePack = () => {
+  const alert = useAlert();
+  const mutation = trpc.pack.delete.useMutation();
+
+  const deletePack = async (packId: string) => {
+    await mutation.mutate(
+      { packId },
+      {
+        onError: (err) => {
+          alert.show(err.message);
+        },
+      }
+    );
+  };
+
+  return { ...mutation, deletePack };
 };
