0628c2d375d011406a66fa8d5a8fd2aef01a3670
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-09-25 23:34:01 -0700

    verify user locally

--- web/src/server/router/index.ts
+++ web/src/server/router/index.ts
@@ -1,10 +1,8 @@
 import { t } from "~/server/trpc";
-import { authRouter } from "./auth";
 import { packRouter } from "./pack";
 import { gameRouter } from "./game";
 
 export const appRouter = t.router({
-  auth: authRouter,
   pack: packRouter,
   game: gameRouter,
 });
