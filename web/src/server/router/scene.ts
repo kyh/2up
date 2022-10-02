545c91692472dfb72458f47bfa40f6b93bbbf3a9
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-10-01 22:11:47 -0700

    create scene router backend

---
+++ web/src/server/router/scene.ts
@@ -0,0 +1,65 @@
+import { z } from "zod";
+import { t, authedProcedure } from "~/server/trpc";
+import { AnswerType, QuestionType } from "@prisma/client";
+
+const sceneModel = z.object({
+  id: z.string().optional(),
+  questionDescription: z.string().optional(),
+  questionType: z.string(),
+  question: z.string(),
+  answerType: z.string(),
+  answers: z.array(
+    z.object({
+      id: z.string().optional(),
+      content: z.string(),
+      isCorrect: z.boolean(),
+    })
+  ),
+  packId: z.string().optional(),
+});
+
+export const sceneRouter = t.router({
+  create: authedProcedure.input(sceneModel).mutation(async ({ ctx, input }) => {
+    return ctx.prisma.scene.create({
+      data: {
+        questionDescription: input.questionDescription,
+        questionType: input.questionType as QuestionType,
+        question: input.question,
+        answerType: input.answerType as AnswerType,
+        answers: {
+          create: input.answers,
+        },
+        pack: {
+          connect: {
+            id: input.packId,
+          },
+        },
+      },
+    });
+  }),
+  update: authedProcedure.input(sceneModel).mutation(async ({ ctx, input }) => {
+    return ctx.prisma.scene.update({
+      where: {
+        id: input.id,
+      },
+      data: {
+        questionDescription: input.questionDescription,
+        questionType: input.questionType as QuestionType,
+        question: input.question,
+        answerType: input.answerType as AnswerType,
+        answers: {
+          upsert: input.answers.map((answer) => ({
+            where: { id: answer.id },
+            create: answer,
+            update: answer,
+          })),
+        },
+      },
+    });
+  }),
+  delete: authedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return;
+    }),
+});
