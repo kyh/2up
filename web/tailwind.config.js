c021be4172f7c9e81d4bb517d1b9b25175a9b5c6
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-02-10 11:23:47 -0500

    Mifrated the Card component - formated the tailwind.config file

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
@@ -199,4 +199,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
