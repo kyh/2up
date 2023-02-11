02621b2becce1351d216b9ac73217fe5ac5d1add
Pejman Ghavami <pejmanghavami@gmail.com>
2023-02-11 11:05:17 -0500

    Migrated the Carousel and Code components to Taiwlind - In the taiwlind config file defined a new font family for the Code component, requred the tailwind-scrollbar plugin and defined some selectors to target children of an element

--- web/src/components/Carousel/Carousel.tsx
+++ web/src/components/Carousel/Carousel.tsx
@@ -1,27 +1,7 @@
-import { theme } from "~/styles/theme";
-import styled from "styled-components";
+import { classed } from "@tw-classed/react";
 
-export const Carousel = styled.div`
-  overflow: auto;
-  display: flex;
-  scroll-snap-type: x mandatory;
-  scrollbar-width: none;
-  gap: ${theme.spacings(5)};
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  & > * {
-    width: 20rem;
-    scroll-snap-align: start;
-    scroll-snap-stop: always;
-    flex-shrink: 0;
-    animation: none !important;
-  }
-
-  & > *:active,
-  & > *:hover {
-    animation: none !important;
-  }
-`;
+export const Carousel = classed.div(
+  "overflow-auto flex snap-mandatory snap-x scrollbar-none gap-5",
+  "child:w-[20rem] child:snap-start child:snap-always child:shrink-0",
+  "child:!animate-none child-hover:!animate-none child-active:!animate-none"
+);
