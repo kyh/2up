9b696f0f6f8d095470d0e599525769f4d2bedd1d
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-02-11 11:00:08 -0500

    Made the Card component use normal borders instead of border imges

--- web/src/components/Card/Card.tsx
+++ web/src/components/Card/Card.tsx
@@ -1,13 +1,7 @@
 import { classed } from "@tw-classed/react";
-import { theme } from "~/styles/theme";
 
 export const Card = classed.div(
-  "flex flex-col p-8",
-  "[border-image-slice:4_4_3_5_fill]",
-  "[border-image-width:5px]",
-  "[border-image-outset:0]",
-  "[border-image-repeat:stretch_stretch]",
-  `[border-image-source:${theme.ui.cardBorderUrl}]`, {
+  "flex flex-col p-8 border-2 border-black dark:border-white", {
   variants: {
     background: {
       true: "bg-white dark:bg-inherit",
