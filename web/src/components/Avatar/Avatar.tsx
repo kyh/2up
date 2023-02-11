725540a318b414cb1e36924a33e183b3262d2d30
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-02-11 12:21:23 -0500

    Migrated the Avatar component to Tailwind - In the HomeSetName lib component changed the way the Avatar component is being used as a quick fix so the app wouldn't break - In tailwind config file defined a path selector to target the fill color

--- web/src/components/Avatar/Avatar.tsx
+++ web/src/components/Avatar/Avatar.tsx
@@ -1,10 +1,5 @@
-import styled, {
-  css,
-  StyledComponentProps,
-  DefaultTheme,
-} from "styled-components";
+import { classed, deriveClassed, ComponentProps } from "@tw-classed/react";
 import { hashCode } from "~/utils/string";
-import { theme } from "~/styles/theme";
 
 import svg0 from "./svgs/0.svg";
 import svg1 from "./svgs/1.svg";
@@ -24,15 +19,23 @@ import svg14 from "./svgs/14.svg";
 import svg15 from "./svgs/15.svg";
 import svg16 from "./svgs/16.svg";
 
-type Props = StyledComponentProps<
-  "div",
-  DefaultTheme,
-  {
-    name: string;
-    expandOnDesktop?: boolean;
-  },
-  never
->;
+const StyledIcon = classed.div(
+  "flex justify-center items-end rounded-full overflow-hidden p-2 border-2 border-black dark:border-white",{
+    variants:{
+      type: {
+        default: "w-20 h-20",
+        setName: "w-28 h-28 desktop:w-24 desktop:h-24 mb-2"
+      }
+    },
+    defaultVariants:{
+      type: "default"
+    }
+  }
+);
+
+type Props = ComponentProps<typeof StyledIcon> & {
+  name: string;
+};
 
 export const avatarMap = {
   0: svg0,
@@ -56,54 +59,15 @@ export const avatarMap = {
 
 const length = Object.keys(avatarMap).length;
 
-export const Avatar = ({ name, expandOnDesktop = true, ...rest }: Props) => {
-  const avatar = hashCode(name, length);
-  const Svg = avatarMap[avatar as keyof typeof avatarMap];
-  if (!Svg) return null;
-  return (
-    <StyledIcon expandOnDesktop={expandOnDesktop} {...rest}>
-      <Svg />
-    </StyledIcon>
-  );
-};
-
-const StyledIcon = styled.div<{ expandOnDesktop: boolean }>`
-  display: flex;
-  width: 70px;
-  height: 70px;
-  border-radius: 100%;
-  overflow: hidden;
-  justify-content: center;
-  align-items: flex-end;
-  padding: ${theme.spacings(1)};
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-image: ${theme.ui.buttonFabBorderUrl};
-
-  > svg {
-    width: 100%;
-    height: 80%;
-    path {
-      fill: ${theme.ui.text};
-    }
+export const Avatar = deriveClassed<typeof StyledIcon, Props>(
+  ({ name, ...rest }, ref) => {
+    const avatar = hashCode(name, length);
+    const Svg = avatarMap[avatar as keyof typeof avatarMap];
+    if (!Svg) return null;
+    return (
+      <StyledIcon {...rest} ref={ref}>
+        <Svg className="w-full h-4/5 desktop:h-full svg-path:fill-black dark:svg-path:fill-white" />
+      </StyledIcon>
+    );
   }
-
-  ${theme.breakpoints.desktop} {
-    ${({ expandOnDesktop }) =>
-      expandOnDesktop &&
-      css`
-        display: inline-flex;
-        width: auto;
-        height: auto;
-        border-radius: 0;
-        overflow: hidden;
-        background: none;
-        overflow: auto;
-
-        > svg {
-          width: 100%;
-          height: 100%;
-        }
-      `}
-  }
-`;
+);
