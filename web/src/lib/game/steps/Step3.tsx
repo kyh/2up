kyh/2up

commit 08bc8b0b069edbe13ae072c3cf3215c95abc7fd8

Fixed an issue related to PlayerGrid.tsx

web/src/lib/game/steps/Step3.tsx
@@ -96,7 +96,7 @@ export const PlayerScores = ({
       <TitleContainer>
         <h2 className="m-0 text-center">{title}</h2>
       </TitleContainer>
-      <PlayersContainer singleCol>
+      <PlayersContainer>
         {sortedPlayers.map(({ playerId, playerName, prevScore, score }) => {
           return (
             <PlayerContainer
@@ -181,10 +181,10 @@ const TitleContainer = classed.div(
 
 const PlayersContainer = classed(
     PlayersGrid,
-    "mx-auto mb-5 max-w-[300px] desktop:mb-0 desktop:max-w-none"
+    "mx-auto mb-5 max-w-[300px] desktop:mb-0 desktop:max-w-none grid-cols-1"
   );
 
-const APC = classed(motion.div, "flex justify-between items-center");
+const APC = classed(motion.div, "flex justify-between items-center w-full");
 
 const PC = classed.div(
   "flex flex-col-reverse justify-between items-center",

web/src/lib/game/GameLobby.tsx
@@ -139,7 +139,7 @@ const ShareIcon = classed(Icon, "relative left-[-10px]");
 
 const PlayersContainer = classed(
   PlayersGrid,
-  "[&_.avatar]:scale-0 [&_.avatar]:animate-[bounce-in_1s_linear_forwards_0.4s]"
+  "[&_.avatar]:scale-0 [&_.avatar]:animate-[bounce-in_1s_linear_forwards_0.4s] grid-cols-3"
 );
 
 const Explosion = classed(AnimationSprite, "top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2");

web/src/lib/game/components/PlayerGrid.tsx
@@ -38,17 +38,8 @@ export const PlayersGrid = classed.div(
   "[&_.player_.name]:desktop:mb-1 [&_.player_.avatar]:desktop:overflow-hidden",
   "[&_.player_.avatar_svg]:desktop:max-w-[130px] [&_.player_.avatar_svg]:desktop:max-h-[300px]",
   "[&_.player_.correct]:desktop:top-auto [&_.player_.correct]:desktop:left-auto [&_.player_.correct]:desktop:bottom-[-7px]",
-  "[&_.avatar_>_svg]:animate-[jitter_0.7s_linear_infinite]", {
-  variants:{
-    singleCol:{
-      true:"grid-cols-1",
-      false:"grid-cols-3"
-    },
-  },
-  defaultVariants:{
-    singleCol:"false"
-  }
-});
+  "[&_.avatar_>_svg]:animate-[jitter_0.7s_linear_infinite]"
+  );
 
 export const NextButton = classed(
   Button, "absolute bottom-10 left-1/2 w-[150px] ml-[-75px] z-10 desktop:bg-white desktop:dark:bg-black px-8");

web/src/lib/game/steps/Step2.tsx
@@ -213,7 +213,7 @@ const SubmissionsContainer = classed(
     PlayersGrid,
     "p-5 desktop:py-0 desktop:animate-[fade-up-in_0.8s_cubic-bezier(0.77,_0.1,_0.46,_1.22)_forwards]",
     "[&_.correct]:absolute [&_.correct]:-top-3 [&_.correct]:left-0",
-    "[&_.name]:text-center"
+    "[&_.name]:text-center grid-cols-3"
   );
 
 const Stars = classed(AnimationSprite, "top-10 desktop:top-5 left-1/2 scale-50 -translate-x-1/2 -translate-y-1/2");

commit 592c335eb28b119528b0c2f5005561162c9776c1

Fixed a bug that was caused by 08bc8b0b0

web/src/lib/game/steps/Step3.tsx
@@ -181,7 +181,7 @@ const TitleContainer = classed.div(
 
 const PlayersContainer = classed(
     PlayersGrid,
-    "mx-auto mb-5 max-w-[300px] desktop:mb-0 desktop:max-w-none grid-cols-1"
+    "mx-auto mb-5 max-w-[300px] desktop:mb-0 desktop:max-w-none desktop:grid-cols-3"
   );
 
 const APC = classed(motion.div, "flex justify-between items-center w-full");