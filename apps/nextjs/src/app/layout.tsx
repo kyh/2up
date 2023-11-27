7a36d3b16dd838d350ac7bc1ea8ce66c2c856411
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-11-27 15:03:43 +0800

    cleanup

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "@/styles/globals.css";
+import "./globals.css";
 
 import { cache } from "react";
 import { headers } from "next/headers";
