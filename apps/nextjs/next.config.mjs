556dc3854499022070203ec365cd3a58110f98e0
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-11-29 23:49:12 +0800

    remove env configs, move files to src

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,7 +1,3 @@
-// Importing env files here to validate on build
-import "./src/env.mjs";
-import "@acme/auth/env.mjs";
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
