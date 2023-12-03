9401b78e6ef7b7ed97142d457b6b968bf44f91bf
Kaiyu Hsu <im.kaiyu@gmail.com>
2023-12-04 06:01:47 +0800

    custom adapter

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,14 +2,18 @@ import { PrismaClient } from "@prisma/client/edge";
 
 const NODE_ENV = process.env.NODE_ENV ?? "development";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     log: NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
+};
+
+export type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (NODE_ENV !== "production") globalForPrisma.prisma = db;
