7726386d28d626d64c27bd4927ceb2c2cf06b652
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-01-05 14:48:52 -0800

    update packages

--- apps/nextjs/src/app/auth/layout.tsx
+++ apps/nextjs/src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -9,17 +10,21 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className="container relative grid min-h-dvh	flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
     <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
       <div className="absolute inset-0 bg-zinc-900" />
-      <div className="relative z-20 flex items-center text-lg font-medium">
+      <Link
+        className="relative z-20 flex items-center text-lg font-medium"
+        href="/"
+      >
         Acme Inc
-      </div>
+      </Link>
       <div className="relative z-20 mt-auto">
         <blockquote className="space-y-2">
           <p className="text-lg">
-            &ldquo;This library has saved me countless hours of work and helped
-            me deliver stunning designs to my clients faster than ever
-            before.&rdquo;
+            &ldquo;Aenean consectetur a enim ac posuere. Pellentesque vehicula
+            semper blandit. Aliquam maximus ligula quis risus porta, sit amet
+            pulvinar mi elementum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.&rdquo;
           </p>
-          <footer className="text-sm">Sofia Davis</footer>
+          <footer className="text-sm">Lorem Ipsum</footer>
         </blockquote>
       </div>
     </div>
