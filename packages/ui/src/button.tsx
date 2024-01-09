cbc54c2355c09ca44184ff429235a88055afffe4
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-01-08 16:17:29 -0800

    add interactive ui to auth

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,9 +4,10 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
 
 import { cn } from ".";
+import { Spinner } from "./spinner";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "relative inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -37,18 +38,44 @@ const buttonVariants = cva(
 
 type ButtonProps = {
   asChild?: boolean;
+  loading?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>;
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      children,
+      loading,
+      disabled,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        disabled={disabled ?? loading}
         {...props}
-      />
+      >
+        <span
+          className={cn(
+            "absolute inset-0 grid place-items-center opacity-0 transition",
+            loading && "opacity-1",
+          )}
+        >
+          <Spinner />
+        </span>
+        <span className={cn("transition", loading && "opacity-0")}>
+          {children}
+        </span>
+      </Comp>
     );
   },
 );
