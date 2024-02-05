da74e9e92663c9c33c1d37b3397e4fc82737935f
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-02-05 14:47:35 -0800

    update settings

---
+++ apps/nextjs/src/app/(dashboard)/account/profile-form.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@init/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@init/ui/form";
+import { Input } from "@init/ui/input";
+import { toast } from "@init/ui/toast";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const profileFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Name must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Name must not be longer than 30 characters.",
+    }),
+  email: z.string().email(),
+});
+
+type ProfileFormValues = z.infer<typeof profileFormSchema>;
+
+// This can come from your database or API.
+const defaultValues: Partial<ProfileFormValues> = {
+  // name: "Your name",
+  // dob: new Date("2023-01-23"),
+};
+
+export const ProfileForm = () => {
+  const form = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileFormSchema),
+    defaultValues,
+  });
+
+  const onSubmit = (data: ProfileFormValues) => {
+    toast("Submitted");
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="col-span-full flex items-center gap-x-8">
+          <img
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+            className="h-24 w-24 flex-none rounded-lg bg-background object-cover"
+          />
+          <div>
+            <Button variant="secondary">Change avatar</Button>
+            <p className="mt-2 text-xs leading-5 text-muted-foreground">
+              JPG, GIF or PNG. 1MB max.
+            </p>
+          </div>
+        </div>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Your name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the name that will be displayed on your profile and in
+                emails.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormDescription>
+                You can always log in by receiving a magic link to the email
+                associated with your account.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <footer className="flex justify-end">
+          <Button type="submit">Update Profile</Button>
+        </footer>
+      </form>
+    </Form>
+  );
+};
