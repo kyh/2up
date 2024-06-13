d5857bda13f812638bbb650b06e7103e9b507d1e
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-06-13 11:08:01 +0300

    Pass classname to borders cva

--- app/components/ui/Borders.tsx
+++ app/components/ui/Borders.tsx
@@ -1,39 +1,43 @@
 import React from "react";
 import { cva, VariantProps } from "class-variance-authority";
-import clsx from "clsx";
 
-const borders = cva(
-  "pointer-events-none absolute px-3 py-2", 
-  {
-    variants: {
-      variant: {
-        normal: "border-normal",
-        success: "border-success",
-        primary: "border-primary",
-        error: "border-error",
-        disabled: "border-disabled",
-        warning: "border-warning",
-      },
-      position: {
-        top: "left-0 top-[-4px] h-[calc(100%+8px)] w-full border-y-4",
-        left: "left-[-4px] top-0 h-full w-[calc(100%+8px)] border-x-4",
-      }
+const borders = cva("pointer-events-none absolute px-3 py-2", {
+  variants: {
+    variant: {
+      normal: "border-normal",
+      success: "border-success",
+      primary: "border-primary",
+      error: "border-error",
+      disabled: "border-disabled",
+      warning: "border-warning",
     },
-    defaultVariants: {
-      variant: "normal",
-    }
-  }
-);
+    position: {
+      top: "left-0 top-[-4px] h-[calc(100%+8px)] w-full border-y-4",
+      left: "left-[-4px] top-0 h-full w-[calc(100%+8px)] border-x-4",
+    },
+  },
+  defaultVariants: {
+    variant: "normal",
+  },
+});
 
-interface BordersProps extends React.HTMLAttributes<HTMLSpanElement>, VariantProps<typeof borders> {
+interface BordersProps
+  extends React.HTMLAttributes<HTMLSpanElement>,
+    VariantProps<typeof borders> {
   className?: string;
 }
 
 const Borders: React.FC<BordersProps> = ({ variant, className, ...props }) => {
   return (
     <>
-      <span className={clsx(borders({ variant, position: "top" }), className)} {...props}></span>
-      <span className={clsx(borders({ variant, position: "left" }), className)} {...props}></span>
+      <span
+        className={borders({ variant, position: "top", className })}
+        {...props}
+      ></span>
+      <span
+        className={borders({ variant, position: "left", className })}
+        {...props}
+      ></span>
     </>
   );
 };
