ebcbd3eeb7add047042236a2f1f08373b421e4cc
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-06-26 17:20:02 -0300

    refactor: rewrite Progress component from JSX to TSX applying new patterns

--- app/components/NosProgress.jsx
+++ app/components/NosProgress.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-import Progres from "./ui/NosProgres";
+import { Progress } from "./ui/NosProgress";
 
-export default function Progress() {
+export default function ProgressComponents() {
   return (
     <div className="relative box-border flex max-w-full flex-wrap gap-5 border-4 border-black p-10">
       <span className="absolute left-10 top-[-12px] bg-white px-2 text-black">
         Progress
       </span>
 
-      <Progres type="normal" percent="80" />
-      <Progres type="primary" percent="65" />
-      <Progres type="success" percent="50" />
-      <Progres type="warning" percent="35" />
-      <Progres type="error" percent="20" />
-      <Progres type="pattern" percent="50" />
+      <Progress type="normal" percent="80" />
+      <Progress type="primary" percent="65" />
+      <Progress type="success" percent="50" />
+      <Progress type="warning" percent="35" />
+      <Progress type="error" percent="20" />
+      <Progress type="pattern" percent="50" />
     </div>
   );
 }
