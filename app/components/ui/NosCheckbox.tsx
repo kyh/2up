0f3a765ae9a2c490769cbad0ab298b078569d3ad
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-06-30 12:59:19 -0300

    feat: Checkbox could be controlled

--- app/components/ui/NosCheckbox.tsx
+++ app/components/ui/NosCheckbox.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from "react";
 import { cva, VariantProps } from "class-variance-authority";
 
+import { cn } from "@/lib/utils";
+
 const checkboxStyles = cva("m-5 cursor-pointer text-base", {
   variants: {
     variant: {
@@ -35,43 +37,44 @@ type CheckboxProps = VariantProps<typeof checkboxStyles> &
   React.HTMLAttributes<HTMLLabelElement> & {
     label: string;
     name: string;
+    isChecked?: boolean;
+    onChange?: (isChecked: boolean) => void;
   };
 
-export const Checkbox = ({ variant, label, name, ...props }: CheckboxProps) => {
-  const [checked, setChecked] = useState(false);
+export const Checkbox = ({
+  variant,
+  label,
+  name,
+  isChecked,
+  onChange,
+  className,
+  ...props
+}: CheckboxProps) => {
+  const [internalIsChecked, setInternalIsChecked] = useState(false);
+  const isControlled = isChecked !== undefined;
   const classes = checkboxStyles({ variant });
 
-  const checkLogo = (
-    <div
-      className="logo absolute -left-8 -top-[3px] h-[2px] w-[2px] -translate-y-1/2 transform group-active:text-dark-888"
-      style={{
-        boxShadow:
-          "2px 2px, 4px 2px, 6px 2px, 8px 2px, 10px 2px, 12px 2px, 14px 2px, 18px 2px, 20px 2px, 2px 4px, 16px 4px, 18px 4px, 20px 4px, 2px 6px, 14px 6px, 16px 6px, 2px 8px, 4px 8px, 12px 8px, 14px 8px, 2px 10px, 4px 10px, 6px 10px, 10px 10px, 12px 10px, 16px 10px, 2px 12px, 6px 12px, 8px 12px, 10px 12px, 16px 12px, 2px 14px, 8px 14px, 16px 14px, 2px 16px, 4px 16px, 6px 16px, 8px 16px, 10px 16px, 12px 16px, 14px 16px, 16px 16px",
-      }}
-    />
-  );
+  const parsedIsChecked = isControlled ? isChecked : internalIsChecked;
+  const iconBoxClasses = iconBoxShadowStyles({ isChecked: parsedIsChecked });
 
-  const boxLogo = (
-    <div
-      className="logo absolute -left-8 -top-[3px] h-[2px] w-[2px] -translate-y-1/2 transform group-active:text-dark-888"
-      style={{
-        boxShadow:
-          "2px 2px, 4px 2px, 6px 2px, 8px 2px, 10px 2px, 12px 2px, 14px 2px, 16px 2px, 2px 4px, 16px 4px, 2px 6px, 16px 6px, 2px 8px, 16px 8px, 2px 10px, 16px 10px, 2px 12px, 16px 12px, 2px 14px, 16px 14px, 2px 16px, 4px 16px, 6px 16px, 8px 16px, 10px 16px, 12px 16px, 14px 16px, 16px 16px",
-      }}
-    />
-  );
-
-  const handleCheckboxChange = () => setChecked(!checked);
+  const handleCheckboxChange = () => {
+    if (!isControlled) {
+      setInternalIsChecked(!parsedIsChecked);
+    }
+    if (onChange) {
+      onChange(!parsedIsChecked);
+    }
+  };
 
   return (
     <div>
-      <label {...props} htmlFor={name} className={classes}>
+      <label {...props} htmlFor={name} className={cn(classes, className)}>
         <input
           type="checkbox"
           id={name}
           name={name}
           value={label}
-          checked={checked}
+          checked={parsedIsChecked}
           onChange={handleCheckboxChange}
           className="mr-4 h-0 w-0 outline-0"
         />
