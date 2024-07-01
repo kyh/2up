52cbf6c9df7d90a3a2b959dd5b7091786c4a4db0
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-07-01 17:21:23 -0300

    refactor(Container): add containerWrapper and containerHeading cva styles

--- app/components/ui/NosContainer.tsx
+++ app/components/ui/NosContainer.tsx
@@ -1,18 +1,66 @@
 import React from "react";
 import { cva, VariantProps } from "class-variance-authority";
-import clsx from "clsx";
 
 import CornerShapes from "./Corners";
 
-const containerStyles = cva("", {
+const containerWrapperStyles = cva("", {
   variants: {
     variant: {
       normal: "border-dark bg-light text-dark",
       dark: "border-light bg-dark text-light",
     },
+    rounded: {
+      true: "p-1",
+      false: "",
+    },
   },
   defaultVariants: {
     variant: "normal",
+    rounded: false,
+  },
+});
+
+const containerStyles = cva(
+  "relative m-1 box-border flex  flex-wrap gap-5 p-6",
+  {
+    variants: {
+      variant: {
+        normal: "border-dark bg-light text-dark",
+        dark: "border-light bg-dark text-light",
+      },
+      rounded: {
+        true: "",
+        false: "border-4",
+      },
+      heading: {
+        true: "pt-10",
+        false: "py-5",
+      },
+      centered: {
+        true: "text-center",
+        false: "text-left",
+      },
+    },
+    defaultVariants: {
+      variant: "normal",
+    },
+  },
+);
+
+const containerHeadingStyles = cva("absolute top-[-6px] px-2", {
+  variants: {
+    variant: {
+      normal: "bg-light text-dark",
+      dark: "bg-dark text-light",
+    },
+    centered: {
+      true: "left-1/2 -translate-x-1/2",
+      false: "left-1 sm:left-10",
+    },
+  },
+  defaultVariants: {
+    variant: "normal",
+    centered: false,
   },
 });
 
@@ -31,38 +79,23 @@ export const Container = ({
   rounded = false,
   ...props
 }: ContainerProps) => {
-  const classes = containerStyles({ variant });
+  const containerWrapperClasses = containerWrapperStyles({ variant, rounded });
+  const containerClasses = containerStyles({ variant, rounded, centered });
+  const containerHeadingClasses = containerHeadingStyles({
+    variant,
+    centered,
+  });
 
   return (
-    <div className={clsx(classes, rounded && `p-1`)}>
-      <div
-        {...props}
-        className={clsx(
-          classes,
-          rounded ? "" : "border-4",
-          heading ? "pt-10" : "py-5",
-          centered ? "text-center" : "text-left",
-          "relative m-1 box-border flex  flex-wrap gap-5 p-6",
-        )}
-      >
+    <div className={containerWrapperClasses}>
+      <div {...props} className={containerClasses}>
         {rounded &&
           (variant === "dark" ? (
             <CornerShapes type="dark" />
           ) : (
             <CornerShapes type="normal" />
           ))}
-
-        {heading && (
-          <span
-            className={clsx(
-              centered ? `left-1/2 -translate-x-1/2` : `left-1 sm:left-10`,
-              variant === "dark" ? `bg-dark text-light` : `bg-light text-dark`,
-              `absolute top-[-6px] px-2`,
-            )}
-          >
-            {heading}
-          </span>
-        )}
+        {heading && <span className={containerHeadingClasses}>{heading}</span>}
         {children}
       </div>
     </div>
