9708a3722461e7636ce0df517f26ea4b929aa75b
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-07-01 16:58:52 -0300

    feat: use redix dialog to build NosDialog

--- app/components/ui/NosDialog.tsx
+++ app/components/ui/NosDialog.tsx
@@ -1,23 +1,27 @@
 "use client";
 
 import React, { useState } from "react";
+import * as RadixDialog from "@radix-ui/react-dialog";
 import { cva, VariantProps } from "class-variance-authority";
 import clsx from "clsx";
 
 import CornerShapes from "./Corners";
 import { NosBtn } from "./NosBtn";
 
-const dialogStyles = cva("relative flex flex-col justify-center gap-3 p-7", {
-  variants: {
-    variant: {
-      normal: "bg-light text-dark",
-      dark: "bg-dark text-light",
+const dialogStyles = cva(
+  "bg-background fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
+  {
+    variants: {
+      variant: {
+        normal: "bg-light text-dark",
+        dark: "bg-dark text-light",
+      },
+    },
+    defaultVariants: {
+      variant: "normal",
     },
   },
-  defaultVariants: {
-    variant: "normal",
-  },
-});
+);
 
 const dialogBorderStyles = cva("border-4", {
   variants: {
@@ -35,6 +39,7 @@ type DialogProps = VariantProps<typeof dialogStyles> &
   React.HTMLAttributes<HTMLUListElement> & {
     label: string;
     alert: string;
+    title?: string;
     block?: boolean;
     rounded?: boolean;
     onConfirm: () => void;
@@ -44,6 +49,7 @@ export const Dialog = ({
   variant,
   label,
   alert,
+  title,
   onConfirm,
   block,
   rounded,
@@ -52,26 +58,43 @@ export const Dialog = ({
   const classes = dialogStyles({ variant });
   const borderClasses = dialogBorderStyles({ variant });
 
-  const handleOpenDialog = () => {
-    setDialogOpen(true);
-  };
+  const handleOpenDialog = () => setDialogOpen(true);
 
-  const handleCloseDialog = () => {
-    setDialogOpen(false);
-  };
+  const handleCloseDialog = () => setDialogOpen(false);
 
   return (
-    <div>
-      <NosBtn variant="primary" block={block} onClick={handleOpenDialog}>
-        {label}
-      </NosBtn>
-
-      {isDialogOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-dark bg-opacity-40">
-          <div className={clsx(classes, rounded || borderClasses)}>
-            <p>Dialog</p>
-            <p className="max-w-sm">Alert: {alert}</p>
-
+    <RadixDialog.Root open={isDialogOpen} onOpenChange={setDialogOpen}>
+      <RadixDialog.Trigger asChild>
+        <div>
+          <NosBtn variant="primary" block={block} onClick={handleOpenDialog}>
+            {label}
+          </NosBtn>
+        </div>
+      </RadixDialog.Trigger>
+      <RadixDialog.Portal>
+        <RadixDialog.Overlay className="fixed inset-0 z-50 bg-dark bg-opacity-40 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+        <RadixDialog.Content
+          className={clsx(classes, rounded || borderClasses)}
+        >
+          {rounded &&
+            (variant === "dark" ? (
+              <CornerShapes type="dark" />
+            ) : (
+              <CornerShapes type="normal" />
+            ))}
+          <RadixDialog.Title className="DialogTitle">
+            <p className="text-lg">{title ?? "Dialog"}</p>
+          </RadixDialog.Title>
+          <RadixDialog.Description className="DialogDescription">
+            <p className="max-w-sm text-base">Alert: {alert}</p>
+          </RadixDialog.Description>
+          <div
+            style={{
+              display: "flex",
+              marginTop: 25,
+              justifyContent: "flex-end",
+            }}
+          >
             <div className="flex justify-center gap-2">
               <NosBtn variant="normal" onClick={handleCloseDialog}>
                 Cancel
@@ -81,16 +104,9 @@ export const Dialog = ({
                 Confirm
               </NosBtn>
             </div>
-
-            {rounded &&
-              (variant === "dark" ? (
-                <CornerShapes type="dark" />
-              ) : (
-                <CornerShapes type="normal" />
-              ))}
           </div>
-        </div>
-      )}
-    </div>
+        </RadixDialog.Content>
+      </RadixDialog.Portal>
+    </RadixDialog.Root>
   );
 };
