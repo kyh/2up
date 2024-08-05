5cad35b5b1bafca83ea93a57304362582c78adcb
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-08-04 18:26:39 -0700

    more code cleanup

--- apps/nextjs/src/app/(dashboard)/dashboard/[team]/billing/page.tsx
+++ apps/nextjs/src/app/(dashboard)/dashboard/[team]/billing/page.tsx
@@ -1,6 +1,21 @@
-import { PageHeader } from "@/components/header";
+import { redirect } from "next/navigation";
 
-const Page = async () => {
+import { PageHeader } from "@/components/header";
+import { api } from "@/trpc/server";
+
+type Params = {
+  team: string;
+};
+
+const Page = async ({ params }: { params: Params }) => {
+  const { account } = await api.account.teamWorkspace({
+    slug: params.team,
+  });
+
+  if (!account) {
+    redirect("/dashboard");
+  }
+
   return (
     <main className="flex flex-1 flex-col px-5">
       <PageHeader>Billing</PageHeader>
