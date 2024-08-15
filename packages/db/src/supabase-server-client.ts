61a1e219562a3ee8ed0dd05ca33dec851e4a2367
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-08-14 18:09:49 -0700

    update packages and supabase auth

--- packages/db/src/supabase-server-client.ts
+++ packages/db/src/supabase-server-client.ts
@@ -9,30 +9,14 @@ import type { Database } from "./database.types";
 import { getServiceRoleKey } from "./get-service-role-key";
 import { getSupabaseClientKeys } from "./get-supabase-client-keys";
 
-const serviceRoleKey = getServiceRoleKey();
 const keys = getSupabaseClientKeys();
 
 /**
  * @name getSupabaseServerClient
  * @description Get a Supabase client for use in the Route Handler Routes
  */
-export const getSupabaseServerClient = <GenericSchema = Database>(
-  params = {
-    admin: false,
-  },
-) => {
-  // prevent any caching (to be removed in Next v15)
+export const getSupabaseServerClient = <GenericSchema = Database>() => {
   noStore();
-
-  if (params.admin) {
-    return createClient<GenericSchema>(keys.url, serviceRoleKey, {
-      auth: {
-        persistSession: false,
-        autoRefreshToken: false,
-        detectSessionInUrl: false,
-      },
-    });
-  }
 
   const cookieStore = cookies();
 
@@ -50,6 +34,20 @@ export const getSupabaseServerClient = <GenericSchema = Database>(
           // user sessions.
         }
       },
+    },
+  });
+};
+
+const serviceRoleKey = getServiceRoleKey();
+
+export const getSupabaseAdminClient = <GenericSchema = Database>() => {
+  noStore();
+
+  return createClient<GenericSchema>(keys.url, serviceRoleKey, {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+      detectSessionInUrl: false,
     },
   });
 };
