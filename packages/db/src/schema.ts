9c846d286f71d7eb4f4639c18ff12ec40aaa84ca
Kaiyu Hsu <im.kaiyu@gmail.com>
2024-10-25 20:51:56 -0700

    add in auth schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -178,6 +178,7 @@ import {
   pgTable,
   pgView,
   primaryKey,
+  smallint,
   text,
   timestamp,
   unique,
@@ -191,6 +192,73 @@ const authSchema = pgSchema("auth");
 
 const users = authSchema.table("users", {
   id: uuid("id").primaryKey(),
+  aud: varchar({ length: 255 }),
+  role: varchar({ length: 255 }),
+  email: varchar({ length: 255 }),
+  encryptedPassword: varchar("encrypted_password", { length: 255 }),
+  emailConfirmedAt: timestamp("email_confirmed_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  invitedAt: timestamp("invited_at", { withTimezone: true, mode: "string" }),
+  confirmationToken: varchar("confirmation_token", { length: 255 }),
+  confirmationSentAt: timestamp("confirmation_sent_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  recoveryToken: varchar("recovery_token", { length: 255 }),
+  recoverySentAt: timestamp("recovery_sent_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  emailChangeTokenNew: varchar("email_change_token_new", { length: 255 }),
+  emailChange: varchar("email_change", { length: 255 }),
+  emailChangeSentAt: timestamp("email_change_sent_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  lastSignInAt: timestamp("last_sign_in_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  rawAppMetaData: jsonb("raw_app_meta_data"),
+  rawUserMetaData: jsonb("raw_user_meta_data"),
+  isSuperAdmin: boolean("is_super_admin"),
+  createdAt: timestamp("created_at", { withTimezone: true, mode: "string" }),
+  updatedAt: timestamp("updated_at", { withTimezone: true, mode: "string" }),
+  phone: text().default(sql`NULL`),
+  phoneConfirmedAt: timestamp("phone_confirmed_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  phoneChange: text("phone_change").default(""),
+  phoneChangeToken: varchar("phone_change_token", { length: 255 }).default(""),
+  phoneChangeSentAt: timestamp("phone_change_sent_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  confirmedAt: timestamp("confirmed_at", {
+    withTimezone: true,
+    mode: "string",
+  }).generatedAlwaysAs(sql`LEAST(email_confirmed_at, phone_confirmed_at)`),
+  emailChangeTokenCurrent: varchar("email_change_token_current", {
+    length: 255,
+  }).default(""),
+  emailChangeConfirmStatus: smallint("email_change_confirm_status").default(0),
+  bannedUntil: timestamp("banned_until", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  reauthenticationToken: varchar("reauthentication_token", {
+    length: 255,
+  }).default(""),
+  reauthenticationSentAt: timestamp("reauthentication_sent_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  isSsoUser: boolean("is_sso_user").default(false).notNull(),
+  deletedAt: timestamp("deleted_at", { withTimezone: true, mode: "string" }),
+  isAnonymous: boolean("is_anonymous").default(false).notNull(),
 });
 
 export const appPermissions = pgEnum("AppPermissions", [
@@ -200,31 +268,37 @@ export const appPermissions = pgEnum("AppPermissions", [
   "members.manage",
   "invites.manage",
 ]);
+
 export const billingProvider = pgEnum("BillingProvider", [
   "stripe",
   "lemon-squeezy",
   "paddle",
 ]);
+
 export const notificationChannel = pgEnum("NotificationChannel", [
   "in_app",
   "email",
   "push",
 ]);
+
 export const notificationType = pgEnum("NotificationType", [
   "info",
   "warning",
   "error",
 ]);
+
 export const paymentStatus = pgEnum("PaymentStatus", [
   "pending",
   "succeeded",
   "failed",
 ]);
+
 export const subscriptionItemType = pgEnum("SubscriptionItemType", [
   "flat",
   "per_seat",
   "metered",
 ]);
+
 export const subscriptionStatus = pgEnum("SubscriptionStatus", [
   "active",
   "trialing",
@@ -235,6 +309,7 @@ export const subscriptionStatus = pgEnum("SubscriptionStatus", [
   "incomplete_expired",
   "paused",
 ]);
+
 export const taskLabel = pgEnum("TaskLabel", [
   "bug",
   "feature",
